b4e1dcece4132fb6bf006ce34a1d3f3bbae743e3
ManasGK <manasgughankarthikeyan@gmail.com>
2024-03-02 18:48:44 +0000

    commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ templates = Jinja2Templates(directory="templates")
 db = TinyDB('mydbfile.json')
 
 
-db.insert({"name": "Manas",
+db.insert({"name": "Manass",
            "age":12,
            "Gender": "Male",
            "MostRecentHeartRate": 65,
