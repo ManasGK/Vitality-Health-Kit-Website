5dcc8e67bf8530b0c3ac72933f030117fd89a5e8
ManasGK <manasgughankarthikeyan@gmail.com>
2024-03-07 20:28:30 +0000

    li

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from tinydb import TinyDB, Query
+import redis
+
+
 
 app = FastAPI()
+
+r=redis.Redis(host="redis-10528.c322.us-east-1-2.ec2.cloud.redislabs.com", port=10528, username="default", password="manickam", decode_responses=True)
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -14,43 +19,33 @@ templates = Jinja2Templates(directory="templates")
 
 db = TinyDB('mydbfile.json')
 
+currentUser = "Manas"
 
-db.insert({"name": "Manas",
-           "age":12,
-           "Gender": "Male",
-           "MostRecentHeartRate": 110,
-           "MostRecentBloodPressure" : 90,
-           "MostRecentTemperature" : 39.2})
+TempList = "Temperatures" + currentUser
+RecentTempOfUser = r.lindex(TempList, 0)
+print(RecentTempOfUser)
 
-user = Query()
-data = db.search(user.name == 'Manas')
-data=[{"name": "Manas",
-           "age":12,
-           "Gender": "Male",
-           "MostRecentHeartRate": 70,
-           "MostRecentBloodPressure" : 100,
-           "MostRecentTemperature" : 39.2}]
+HeartRateList = "HeartRates" + currentUser
+RecentHeartRateofUser = r.lindex(HeartRateList, 0)
+print(RecentHeartRateofUser)
 
-print(data)
 
-# For the dashboard
 
 @app.get("/dashboard", response_class=HTMLResponse)
 async def read_item_dashboard(request: Request):
    return templates.TemplateResponse(
        request=request,
        name="home.html",
-       context={"data": data[0],
-                "MostRecentHeartRate": data[0]["MostRecentHeartRate"]}
+       context={"RecentHeartRateofUser" : int(RecentHeartRateofUser), "RecentTempOfUser" : int(RecentTempOfUser)}
    )
 
 # For the results page
 
-@app.get("/results", response_class=HTMLResponse) # Shows that /app is the endpoint (makes it unique page) ---- responseclass=HTMLResponse whows that the output file on the server will be in HTML
-async def read_item(request: Request): # A function to read the item
-   return templates.TemplateResponse( # This Built in Function, processes the template file, and generates the final HTML content
-       request=request,
-       name="about.html",
-       context={"data": data[0]}
-   )
+# @app.get("/results", response_class=HTMLResponse) # Shows that /app is the endpoint (makes it unique page) ---- responseclass=HTMLResponse whows that the output file on the server will be in HTML
+# async def read_item(request: Request): # A function to read the item
+#    return templates.TemplateResponse( # This Built in Function, processes the template file, and generates the final HTML content
+#        request=request,
+#        name="about.html",
+#        context={"data": data[0]}
+#    )
 
